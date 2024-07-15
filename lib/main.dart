f15027ebc1c1add4ac0e5426cabc564ef474b357
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-07-16 03:33:38 +0900

    feat: update style

--- lib/main.dart
+++ lib/main.dart
@@ -352,41 +352,29 @@ class _SegmentedControlExampleState extends State<SegmentedControlExample> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      backgroundColor: skyColors[_selectedSegment],
-      navigationBar: CupertinoNavigationBar(
-        // This Cupertino segmented control has the enum "Sky" as the type.
-        middle: CupertinoSlidingSegmentedControl<Sky>(
-          backgroundColor: CupertinoColors.systemGrey2,
-          thumbColor: skyColors[_selectedSegment]!,
-          // This represents the currently selected segmented control.
-          groupValue: _selectedSegment,
-          // Callback that sets the selected segmented control.
-          onValueChanged: (Sky? value) {
-            if (value != null) {
-              setState(() {
-                _selectedSegment = value;
-              });
-            }
-          },
-          children: const <Sky, Widget>{
-            Sky.midnight: Text(
-              '上り',
-              style: TextStyle(color: CupertinoColors.white),
-            ),
-            Sky.viridian: Text(
-              '下り',
-              style: TextStyle(color: CupertinoColors.white),
-            ),
-          },
+    return CupertinoSlidingSegmentedControl<Sky>(
+      backgroundColor: CupertinoColors.systemGrey2,
+      thumbColor: skyColors[_selectedSegment]!,
+      // This represents the currently selected segmented control.
+      groupValue: _selectedSegment,
+      // Callback that sets the selected segmented control.
+      onValueChanged: (Sky? value) {
+        if (value != null) {
+          setState(() {
+            _selectedSegment = value;
+          });
+        }
+      },
+      children: const <Sky, Widget>{
+        Sky.midnight: Text(
+          '上り',
+          style: TextStyle(color: CupertinoColors.white),
         ),
-      ),
-      child: Center(
-        child: Text(
-          'Selected Segment: ${_selectedSegment.name}',
-          style: const TextStyle(color: CupertinoColors.white),
+        Sky.viridian: Text(
+          '下り',
+          style: TextStyle(color: CupertinoColors.white),
         ),
-      ),
+      },
     );
   }
 }
