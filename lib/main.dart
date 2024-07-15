c9d98d93ead1a66a93c0d25cf6ac986e8784f04f
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-07-16 02:12:02 +0900

    feat: add ListSectionInsetExample2

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,12 @@ class MyApp extends StatelessWidget {
             navigationBar: CupertinoNavigationBar(
               middle: Text('混雑回避アプリ'),
             ),
-            child: SafeArea(child: ListSectionInsetExample()),
+            child: SafeArea(child: Column(
+              children: [
+                ListSectionInsetExample(),
+                ListSectionInsetExample2(),
+              ],
+            )),
           );
         },
         '/route-selection': (BuildContext context) {
@@ -132,6 +137,94 @@ class ListSectionInsetExample extends StatelessWidget {
 
 class _SecondPage extends StatelessWidget {
   const _SecondPage({required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
+
+class ListSectionInsetExample2 extends StatelessWidget {
+  const ListSectionInsetExample2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: CupertinoListSection.insetGrouped(
+        header: const Text('発車駅情報'),
+        children: <CupertinoListTile>[
+          CupertinoListTile.notched(
+            title: const Text('湘南新宿ライン'),
+            leading: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: CupertinoColors.activeGreen,
+            ),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Open pull request');
+                },
+              ),
+            ),
+          ),
+          CupertinoListTile.notched(
+            title: const Text('方向'),
+            leading: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: CupertinoColors.systemRed,
+            ),
+            additionalInfo: const Text('Not available'),
+          ),
+          CupertinoListTile.notched(
+            title: const Text('藤沢'),
+            leading: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: CupertinoColors.activeOrange,
+            ),
+            additionalInfo: const Text('12 days ago'),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Last commit');
+                },
+              ),
+            ),
+          ),
+          CupertinoListTile.notched(
+            title: const Text('7:05'),
+            leading: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: CupertinoColors.activeGreen,
+            ),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Open pull request');
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SecondPage2 extends StatelessWidget {
+  const _SecondPage2({required this.text});
 
   final String text;
 
