b0945c3e3fd4a6324c15ff8f5e9639fb5ebfec3e
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-07-16 11:19:57 +0900

    refactor: move ListSectionInsetExample to file

---
+++ lib/list_section_inset_example.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:quickflow/main.dart';
+
+class ListSectionInsetExample extends StatelessWidget {
+  const ListSectionInsetExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: CupertinoListSection.insetGrouped(
+        header: const Text('発車駅情報'),
+        children: <CupertinoListTile>[
+          CupertinoListTile.notched(
+            title: const Text('湘南新宿ライン'),
+            subtitle: const Text('路線'),
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(7.0),
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: CupertinoColors.activeGreen,
+              ),
+            ),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Open pull request');
+                },
+              ),
+            ),
+          ),
+          CupertinoListTile.notched(
+            title: const Text('方向'),
+            additionalInfo: const SegmentedControlExample(),
+          ),
+          CupertinoListTile.notched(
+            title: const Text('藤沢'),
+            subtitle: const Text('発車駅'),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Open pull request');
+                },
+              ),
+            ),
+          ),
+          CupertinoListTile.notched(
+            title: const Text('7:05'),
+            subtitle: const Text('発車時刻'),
+            trailing: const CupertinoListTileChevron(),
+            onTap: () => Navigator.of(context).push(
+              CupertinoPageRoute<void>(
+                builder: (BuildContext context) {
+                  return const _SecondPage(text: 'Open pull request');
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SecondPage extends StatelessWidget {
+  const _SecondPage({required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
+import 'list_section_inset_example.dart';
+
 enum Sky { midnight, viridian, cerulean }
 
 Map<Sky, Color> skyColors = <Sky, Color>{
@@ -67,84 +69,6 @@ class MyApp extends StatelessWidget {
           );
         }
       },
-    );
-  }
-}
-
-class ListSectionInsetExample extends StatelessWidget {
-  const ListSectionInsetExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: CupertinoListSection.insetGrouped(
-        header: const Text('発車駅情報'),
-        children: <CupertinoListTile>[
-          CupertinoListTile.notched(
-            title: const Text('湘南新宿ライン'),
-            subtitle: const Text('路線'),
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(7.0),
-              child: Container(
-                width: double.infinity,
-                height: double.infinity,
-                color: CupertinoColors.activeGreen,
-              ),
-            ),
-            trailing: const CupertinoListTileChevron(),
-            onTap: () => Navigator.of(context).push(
-              CupertinoPageRoute<void>(
-                builder: (BuildContext context) {
-                  return const _SecondPage(text: 'Open pull request');
-                },
-              ),
-            ),
-          ),
-          CupertinoListTile.notched(
-            title: const Text('方向'),
-            additionalInfo: const SegmentedControlExample(),
-          ),
-          CupertinoListTile.notched(
-            title: const Text('藤沢'),
-            subtitle: const Text('発車駅'),
-            trailing: const CupertinoListTileChevron(),
-            onTap: () => Navigator.of(context).push(
-              CupertinoPageRoute<void>(
-                builder: (BuildContext context) {
-                  return const _SecondPage(text: 'Open pull request');
-                },
-              ),
-            ),
-          ),
-          CupertinoListTile.notched(
-            title: const Text('7:05'),
-            subtitle: const Text('発車時刻'),
-            trailing: const CupertinoListTileChevron(),
-            onTap: () => Navigator.of(context).push(
-              CupertinoPageRoute<void>(
-                builder: (BuildContext context) {
-                  return const _SecondPage(text: 'Open pull request');
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _SecondPage extends StatelessWidget {
-  const _SecondPage({required this.text});
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Center(
-        child: Text(text),
-      ),
     );
   }
 }
