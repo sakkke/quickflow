4d1baca1e6abaea4566b744a1f310300fd4f5146
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-07-17 00:19:13 +0900

    feat: add /api/v1/stations

--- quickflow-server/index.ts
+++ quickflow-server/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 const app = new Hono()
 
+app.use('/api/v1/stations', serveStatic({ path: './api/v1/stations.json' }))
 app.get('/', (c) => c.text('Hono!'))
 
 export default app
